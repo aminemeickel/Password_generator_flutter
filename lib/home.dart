7ec8399dd09976f2fd61a57c0556470dd1fe1a90
Amine Meickel <emine00790@hotmail.com>
2021-04-24 00:10:55 +0100

    final clean

--- lib/home.dart
+++ lib/home.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
             alignment: Alignment.center,
             padding: EdgeInsets.all(10),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 textfield(
                   textContorler: _textContorler,
